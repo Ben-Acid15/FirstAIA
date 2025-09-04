e9f757f5af85dd63c97625b9f26bd7bcd53ad738
Ben-Acid15 <bene15.br@gmail.com>
2025-09-04 15:46:16 +0200

    12 - starting py exe func

---
+++ functions/run_python_file.py
@@ -0,0 +1,9 @@
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File "{file_path}" not found.'
+    if not target_file.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
